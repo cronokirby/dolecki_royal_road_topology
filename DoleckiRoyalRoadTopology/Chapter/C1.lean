6b2b9b704a79a53139693877efb0f536fa6eb939
Lucas Meier <lucas@cronokirby.com>
2024-12-04 23:48:52 -0800

    Tinker some more with relations

--- DoleckiRoyalRoadTopology/Chapter/C1.lean
+++ DoleckiRoyalRoadTopology/Chapter/C1.lean
@@ -1,11 +1,12 @@
 import Mathlib.Data.Set.Defs
 
+
 /-- A relation between two types maps each α to a set of β.
 
 This generalizes functions, by allowing no output for a given value,
 or some output for a given value.
 -/
-structure Rel (α β) where
+structure Rel (α β : Type) where
   image : α → Set β
 
 /-- The opposite relation tells us how the β correspond to sets of α. -/
@@ -13,4 +14,13 @@ def Rel.op (R : Rel α β) : Rel β α := {
   image := fun (b : β) => { a : α | b ∈ R.image a}
 }
 
-#check ∀ (α β : Type) (R : Rel α β), R.op.op = R
+/-- The opposite of the opposite is the same relation. -/
+theorem Rel.op_op_id {R : Rel α β} : R.op.op = R := rfl -- Wow!
+
+def Rel.comp (S : Rel β γ) (R : Rel α β) : Rel α γ := {
+  image := fun (a) => {c | ∃ b ∈ R.image a, c ∈ S.image b }
+}
+
+def Rel.graph (f : α → β) : Rel α β := { image := fun (a) => {f a} }
+
+def Rel.Δ : Rel α α := Rel.graph id
