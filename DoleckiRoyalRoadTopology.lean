42c691c8c32207d3851cc4d36e187140157edac7
Lucas Meier <lucas@cronokirby.com>
2024-12-04 21:27:52 -0800

    Add stub chapter 1

--- DoleckiRoyalRoadTopology.lean
+++ DoleckiRoyalRoadTopology.lean
@@ -1,3 +1,1 @@
--- This module serves as the root of the `DoleckiRoyalRoadTopology` library.
--- Import modules here that should be built as part of the library.
-import DoleckiRoyalRoadTopology.Basic
+import DoleckiRoyalRoadTopology.Chapter.C1
