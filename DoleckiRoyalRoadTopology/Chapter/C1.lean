e0c296211052ccc047e3ad3b31c8ca4ffbdec2f9
Lucas Meier <lucas@cronokirby.com>
2024-12-04 23:11:37 -0800

    Add basic relation definitions

--- DoleckiRoyalRoadTopology/Chapter/C1.lean
+++ DoleckiRoyalRoadTopology/Chapter/C1.lean
@@ -1,1 +1,16 @@
-def x : Nat := 3
+import Mathlib.Data.Set.Defs
+
+/-- A relation between two types maps each α to a set of β.
+
+This generalizes functions, by allowing no output for a given value,
+or some output for a given value.
+-/
+structure Rel (α β) where
+  image : α → Set β
+
+/-- The opposite relation tells us how the β correspond to sets of α. -/
+def Rel.op (R : Rel α β) : Rel β α := {
+  image := fun (b : β) => { a : α | b ∈ R.image a}
+}
+
+#check ∀ (α β : Type) (R : Rel α β), R.op.op = R
