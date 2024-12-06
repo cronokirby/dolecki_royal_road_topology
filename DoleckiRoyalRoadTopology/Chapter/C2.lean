6f0ed15353b99e6b28cf8f564c7f47e7e03fad7c
Lucas Meier <lucas@cronokirby.com>
2024-12-05 22:18:41 -0800

    Principal filters

--- DoleckiRoyalRoadTopology/Chapter/C2.lean
+++ DoleckiRoyalRoadTopology/Chapter/C2.lean
@@ -13,7 +13,6 @@ This is useful in defining the notion of a FilterBase, among other things.
 def Set.upper_closure (X : Set (Set α)) : Set (Set α) :=
   {a : Set α | ∃ x ∈ X, x ⊆ a}
 
-
 variable {X : Set (Set α)}
 
 lemma Set.upper_closure_larger : X ⊆ X.upper_closure := by
@@ -53,9 +52,16 @@ structure Filter (α) where
   -/
   has_univ : Set.univ ∈ sets
 
-class Filter.Proper (F : Filter α): Prop where
+class Filter.Proper (F : Filter α) : Prop where
   no_empty_set : ∅ ∉ F.sets
 
 /-- A collection is a base for a filter, if its upper closure provides the colleciton of sets of F.-/
 def Filter.has_base {F : Filter α} (base : Set (Set α)) : Prop :=
   base.upper_closure = F.sets
+
+/-- A filter is *principal* if there exists a single set generating it.
+
+Because this follows directly from there being a finite base, because of the meet
+property, we instead use the simpler definition.
+-/
+def Filter.Principal {F : Filter α} : Prop := ∃ B : Set α, F.has_base {B}
