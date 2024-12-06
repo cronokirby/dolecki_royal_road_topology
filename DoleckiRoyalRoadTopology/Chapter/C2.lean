f6be16092805e87bfeb91e9c68fba81bf60c019a
Lucas Meier <lucas@cronokirby.com>
2024-12-05 20:49:29 -0800

    Add some basic facts about set upper closures
    These are also in mathlib, but working these out here is nice.

--- DoleckiRoyalRoadTopology/Chapter/C2.lean
+++ DoleckiRoyalRoadTopology/Chapter/C2.lean
@@ -1,12 +1,41 @@
 import Mathlib.Data.Set.Defs
 import Mathlib.Data.Set.Basic
 
+section sets
+/-
+! This section contains a few useful constructions and theorems about sets
+-/
+
 /-- The upper closure of a set contains all elements above that in the original set.
 
 This is useful in defining the notion of a FilterBase, among other things.
 -/
 def Set.upper_closure (X : Set (Set α)) : Set (Set α) :=
   {a : Set α | ∃ x ∈ X, x ⊆ a}
+
+
+variable {X : Set (Set α)}
+
+lemma Set.upper_closure_larger : X ⊆ X.upper_closure := by
+  rw [Set.subset_def]
+  intro x _
+  apply Set.mem_setOf.mpr
+  exists x
+
+lemma Set.upper_closure_idempotent : X.upper_closure.upper_closure = X.upper_closure := by
+  suffices X.upper_closure.upper_closure ⊆ X.upper_closure by
+    apply Set.eq_of_subset_of_subset
+    exact this
+    exact Set.upper_closure_larger
+  rw [Set.subset_def]
+  intro x x_in_X_u_u
+  have ⟨a, ⟨a_in_X_u, a_sub_x⟩⟩ : ∃ a ∈ X.upper_closure, a ⊆ x := x_in_X_u_u.out
+  have ⟨b, ⟨b_in_X, b_sub_a⟩⟩ : ∃ b ∈ X, b ⊆ a := a_in_X_u.out
+  apply Set.mem_setOf.mpr
+  exists b
+  apply And.intro b_in_X
+  exact Set.Subset.trans b_sub_a a_sub_x
+end sets
 
 structure Filter (α) where
   /-- A filter is a collection of subsets of α. -/
